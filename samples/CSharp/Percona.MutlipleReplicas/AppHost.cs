f1d1b32bf5aa0a5a6b4c49b84934cd24dd251919
Pavel Fedarovich <p.fedarovich@gmail.com>
2026-03-30 13:39:41 -0700

    Add Percona.MutlipleReplicas sample.

---
+++ samples/CSharp/Percona.MutlipleReplicas/AppHost.cs
@@ -0,0 +1,31 @@
+﻿using Fedarovich.Aspire.Hosting.MongoDB.ReplicaSet;
+
+#pragma warning disable ASPIRECERTIFICATES001
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var passwordParameter = ParameterResourceBuilderExtensions.CreateDefaultPasswordParameter(builder, "mongo-password", special: false);
+var passwordParameterBuilder = builder.CreateResourceBuilder(passwordParameter);
+
+var mongoServer1 = builder.AddPerconaServerForMongoDB("TestMongo1", 27017, password: passwordParameterBuilder);
+var mongoServer2 = builder.AddPerconaServerForMongoDB("TestMongo2", 27018, password: passwordParameterBuilder);
+var mongoServer3 = builder.AddPerconaServerForMongoDB("TestMongo3", 27019, password: passwordParameterBuilder);
+
+mongoServer1.AddDatabase("TestMongoDB1", "TestDB");
+mongoServer2.AddDatabase("TestMongoDB2", "TestDB");
+mongoServer3.AddDatabase("TestMongoDB3", "TestDB");
+
+var mongoCertificate = MongoDBCertificateUtils.CreateSelfSignedCertificate();
+
+var mongoCertificateAuthority = builder.AddCertificateAuthorityCollection("TestMongoCA")
+    .WithCertificate(mongoCertificate);
+
+var mongoRs = builder.AddMongoDBReplicaSet("TestMongoRS")
+    .WithMember(mongoServer1)
+    .WithMember(mongoServer2)
+    .WithMember(mongoServer3)
+    .WithHttpsCertificate(mongoCertificate)
+    .WithCertificateAuthorityCollection(mongoCertificateAuthority)
+    .WithDbGate();
+
+builder.Build().Run();
